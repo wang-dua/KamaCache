6079da74e0971a652385859cff8a4919edff9263
wjx <wjxuan22@foxmail.com>
2025-07-01 11:22:09 +0800

    添加项目文件。

---
+++ KICachePolicy.h
@@ -0,0 +1,13 @@
+#pragma once
+namespace {
+	template <typename Key, typename Value>
+	class KICachePolicy
+	{
+	public:
+		virtual ~KICachePolicy() = default;
+		virtual void put(Key key, Value value) = 0; //纯虚函数 派生类必须实现
+		virtual bool get(Key key, Value& value) = 0;
+		virtual Value get(Key key) = 0;
+	};
+}
+
